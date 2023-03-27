a21ee4f42346933dd2638d028e0bfc920d60f770
oleg <legik7127@gmail.com>
2023-03-27 22:46:34 +0600

    added block try with resources

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,23 +3,12 @@ import java.io.IOException;
 
 public class App {
     public static void main(String[] args) {
-        FileReader test = null;
-        try {
-            test = new FileReader("/Users/olegmonogarov/IdeaProjects/Exceptions_lecture_2/gradlew");
+        try (FileReader test = new FileReader("/Users/olegmonogarov/IdeaProjects/Exceptions_lecture_2/gradlew")){
             test.read();
         } catch (RuntimeException | IOException e) {
             System.out.println("catch exception: " + e.getClass().getSimpleName());
         }
-        finally {
-            System.out.println("finally start");
-            if (test != null){
-                try {
-                    test.close();
-                } catch (IOException e) {
-                    System.out.println("exception while close");;
-                }
-            }
-            System.out.println("finally finished");
+
         }
     }
 }

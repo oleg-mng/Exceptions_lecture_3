47368270bd58f93903989c1854ae82a41337a831
oleg <legik7127@gmail.com>
2023-03-27 23:53:19 +0600

    added FileReader & FileWriter

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,14 +1,19 @@
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class App {
     public static void main(String[] args) {
-        try (FileReader test = new FileReader("/Users/olegmonogarov/IdeaProjects/Exceptions_lecture_2/gradlew")){
-            test.read();
+        try (FileReader reader = new FileReader("/Users/olegmonogarov/IdeaProjects/Exceptions_lecture_2/gradlew");
+             FileWriter writer = new FileWriter("test")) {
+            while (reader.ready()){
+                writer.write(reader.read());
+            }
         } catch (RuntimeException | IOException e) {
             System.out.println("catch exception: " + e.getClass().getSimpleName());
         }
+        System.out.println("copy successfully");
 
-        }
     }
 }
+
